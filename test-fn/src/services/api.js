23bf7c2a9493078403d6ba80f435bde64b049e48
JDMUHIRWA <j.hareriman@alustudent.com>
2025-03-12 20:40:43 +0200

    chore: Update API base URL to use VITE_API_URL

--- test-fn/src/services/api.js
+++ test-fn/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+console.log(import.meta.env.VITE_API_URL);
+
 const api = axios.create({
-  baseURL: import.meta.env.REACT_APP_API_URL || "http://localhost:7001/api",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:7001/api",
 });
 
 export default api;
