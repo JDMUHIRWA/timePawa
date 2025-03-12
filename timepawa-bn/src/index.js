4d5ac21c736dd56eea367c72ee392c315f36a2c4
JDMUHIRWA <j.hareriman@alustudent.com>
2025-03-12 20:29:26 +0200

    chore: Update CORS configuration in index.js to allow requests from any origin

--- timepawa-bn/src/index.js
+++ timepawa-bn/src/index.js
@@ -25,13 +25,13 @@ dbConnect();
 
 const app = express();
 
-//middlewares
-const corsOptions = {
-  origin: ["http://localhost:3001"],
-  credentials: true,
-};
+// //middlewares
+// const corsOptions = {
+//   origin: ["http://localhost:3001"],
+//   credentials: true,
+// };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
 app.use(
