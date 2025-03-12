4c50f9f700b92b64b3164de28d45a4e7e8122cc5
JDMUHIRWA <j.hareriman@alustudent.com>
2025-03-12 17:47:41 +0200

    chore: Update API base URL in api.js
    This commit updates the API base URL in the `api.js` file to use the `import.meta.env.REACT_APP_API_URL` variable instead of `process.env.REACT_APP_API_URL`. This change ensures that the correct API URL is used when making requests in the application.

--- test-fn/src/services/api.js
+++ test-fn/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
-import process from "process";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:7001/api",
+  baseURL: import.meta.env.REACT_APP_API_URL || "http://localhost:7001/api",
 });
 
 export default api;
