4c1c802dc20e400f757234bbf9aa7cd04c08f075
JDMUHIRWA <j.hareriman@alustudent.com>
2025-03-13 17:56:48 +0200

    added the proxy

--- timepawa-bn/src/index.js
+++ timepawa-bn/src/index.js
@@ -32,15 +32,11 @@ dbConnect();
 const app = express();
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:3001",
-      "https://timepawa.vercel.app",
-      "https://timepawa.onrender.com",
-    ],
+    origin: ["http://localhost:3001", "https://timepawa.vercel.app"],
     credentials: true,
   })
 );
+app.set("trust proxy", 1);
 
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
