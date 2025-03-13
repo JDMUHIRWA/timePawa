0b1baf5163fc0c2c605c4bf12d79ea9daef1f1ff
JDMUHIRWA <j.hareriman@alustudent.com>
2025-03-13 17:56:22 +0200

    `chore: Update API base URL to use VITE_API_URL and remove unused code`

--- test-fn/src/services/api.js
+++ test-fn/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://timepawa.onrender.com/api",
-  baseURL: "http://localhost:7001/api",
+  baseURL: "https://timepawa.onrender.com/api",
+  // baseURL: "http://localhost:7001/api",
   withCredentials: true,
 });
 
